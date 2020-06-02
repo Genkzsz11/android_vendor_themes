fc747dbc6df48877e93bdc6a723dfbe79b751ee8
Genkzsz11 <Genkzsz11@gmail.com>
2020-06-03 03:51:25 +0700

    Revert "vendor: notch-city: Add 3 mode display cutout handler [2/3]"
    This reverts commit bec66b2304103425017195deb94b2ed8f47f980c.

--- overlays.mk
+++ overlays.mk
@@ -32,11 +32,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     MatchmakerOverlay
 
-# Cutout Overlay
-PRODUCT_PACKAGES += \
-    HideCutout \
-    StatusBarStock
-
 # Themes
 PRODUCT_PACKAGES += \
     PitchBlackSystemOverlay \
